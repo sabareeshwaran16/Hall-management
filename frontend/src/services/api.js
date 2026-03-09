12207cf7db408bddcf73e8cc92187be72351a901
sabareeshwaran16 <sabareesh4089@gmail.com>
2026-03-09 21:37:32 +0530

    Add production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://hall-backend-ppr6.onrender.com/api';
+
 
 // Create axios instance
 const api = axios.create({
